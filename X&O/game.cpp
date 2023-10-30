0c0d6a3bfe4e7b2d0f2eb8f4fb6cb3732772bec5
garga <garga@DESKTOP-93KN2ET>
2023-10-30 17:03:18 +0200

    Добавьте файлы проекта.

---
+++ X&O/game.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char field[3][3]; //x & o
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << field[i][j];
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
